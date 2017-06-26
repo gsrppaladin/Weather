96d30c1f53314e20bfb4aa3f6c87c553f1fd0aa3
Sam Greenhill <samgreenhill@DESKTOP-K039ABH.home>
2017-06-25 20:39:53 -0400

    Just a little boiler plate stuff

---
+++ Weather/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Weather
+//
+//  Created by Sam Greenhill on 6/25/17.
+//  Copyright © 2017 simplyAmazingMachines. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
